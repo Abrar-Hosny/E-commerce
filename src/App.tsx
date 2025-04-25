306b2097837cbd570aabfd184f27311cbe9806f1
Abrar-Gaber <abrarhosnygabermohamed.com>
2025-04-25 16:36:04 +0300

    feat: integrate Flowbite React and enhance App component
    - Added Flowbite React as a dependency and configured postinstall script.
    - Updated App component with a welcoming header and description.
    - Modified index.css to include Flowbite React plugin and class list.
    - Updated Vite configuration to include Flowbite React plugin.
    - Created .flowbite-react directory with configuration and gitignore files.
    - Added VSCode extensions recommendations for Tailwind CSS support.

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,24 @@
-import { useState } from 'react'
+import { useState } from "react";
+import "./index.css";
 
 
 function App() {
-
   return (
     <>
-
-
+      <div className="bg-blue-50 h-screen">
+        <header className="text-center ">
+          <div className="pb-3">
+            <h1 className="text-4xl font-bold text-center ">
+              Welcome to <span className="text-blue-700">Yoo Shop</span>
+            </h1>
+          </div>
+          <p>Discover our amazing collection of high-quality products.</p>
+          <p>From electronics to fashion, we have something for everyone!</p>
+        </header>
+        
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
