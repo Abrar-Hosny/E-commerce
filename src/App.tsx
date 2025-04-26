62d0f9add7120f1b390299b133df95a3396d7cae
Abrar-Hosny <abrarhosny19@gmail.com>
2025-04-26 13:36:16 +0300

    Refactor: clean up App and Layout components for improved readability and structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,20 @@
 import "./index.css";
 import "flowbite";
-import { BrowserRouter , Routes , Route } from "react-router-dom";
-
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Home from "./Home";
 import Layout from "./Layout";
 
-
 function App() {
   return (
     <>
-  <BrowserRouter>
-  <Routes>
-<Route path="/"  element={<Layout/>}>
-<Route index element={<Home/>} />
-
-
-</Route>
-  
-  </Routes>
-  
-  </BrowserRouter>
-     
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
