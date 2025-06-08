3011f667f2ec20adebf5d946b459bbcb092e73e6
Abrar-Hosny <abrarhosny19@gmail.com>
2025-06-08 13:58:37 +0300

    adjust grid layout and styling in Shop component for improved presentation

--- src/Features/shop/Shop.tsx
+++ src/Features/shop/Shop.tsx
@@ -17,15 +17,15 @@ export default function Shop() {
       </div>
 
       <div>
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6 mt-8 mb-8 px-4">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-2  gap-6 mt-8 mb-8 px-4">
           {products.map((product) => (
             <div
               key={product.id}
-              className="rounded-xl overflow-hidden shadow-lg bg-white hover:shadow-2xl transition-all duration-300 transform hover:-translate-y-1"
+              className="rounded-xl overflow-hidden shadow-lg border hover:shadow-2xl transition-all duration-300 transform hover:-translate-y-1"
             >
-              <div className="relative overflow-hidden bg-gray-50 flex items-center justify-center">
+              <div className="relative overflow-hidden  flex items-center justify-center">
                 <img
-                  className="w-full h-72 object-contain p-4 transition-transform duration-300 ease-in-out hover:scale-105"
+                  className="w-72 h-72 object-contain p-4 transition-transform duration-300 ease-in-out hover:scale-105"
                   src={product.image}
                   alt={product.title}
                   loading="lazy"
